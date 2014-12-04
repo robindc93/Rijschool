cbffb9fc1226bc2bac7f83b9a72b507f226ea2a7
kennethjeu <kenneth.vandenberghe@student.thomasmore.be>
2014-12-04 20:59:50 +0100

    Leeg MVC project met individuele accounts

---
+++ Rijschool/Rijschool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rijschool.Startup))]
+namespace Rijschool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
