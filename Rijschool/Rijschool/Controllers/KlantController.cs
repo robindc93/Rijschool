a072a893c338e2869ce03914f36f8319bf06c431
kennethjeu <kenneth.vandenberghe@student.thomasmore.be>
2014-12-06 14:44:26 +0100

    Registreer Klant

---
+++ Rijschool/Rijschool/Controllers/KlantController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNet.Identity;
+using Rijschool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rijschool.Controllers
+{
+    public class KlantController : AccountController
+    {
+        
+        // GET: Klant
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Registreer()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Registreer(KlantVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new Klant { 
+                    Voornaam = model.Voornaam,
+                    Familienaam = model.Familienaam,
+                    Adres = model.Familienaam,
+                    Gemeente = model.Gemeente,
+                    Email = model.Email,
+                    UserName = model.Email,
+                    KlantSedert = DateTime.Now
+                };
+                var result = await UserManager.CreateAsync(user, model.Wachtwoord);
+                if (result.Succeeded)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+    }
+}
---
+++ Rijschool/Rijschool/Models/KlantVM.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Rijschool.Models
+{
+    public class KlantVM
+    {
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Voornaam")]
+        public string Voornaam { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Familienaam")]
+        public string Familienaam { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Straat en huisnummer")]
+        public string Adres { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Gemeente")]
+        public string Gemeente { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Het wachtwoord moet minstens uit {2} tekens bestaan.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Wachtwoord")]
+        public string Wachtwoord { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Bevestig wachtwoord")]
+        [Compare("Wachtwoord", ErrorMessage = "Het wachtwoord en bevestigde wachtwoord komen niet overeen.")]
+        public string BevestigWachtwoord { get; set; }
+    }
+}
