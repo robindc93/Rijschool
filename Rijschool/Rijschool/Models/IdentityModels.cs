6125481b71dd126ea6b0103af6eb7018fcfbf24b
kennethvdberghe <kenneth.vdb@hotmail.com>
2014-12-11 16:38:48 +0100

    ConnectionString veranderd van "Default" naar "Rijschool"

--- Rijschool/Rijschool/Models/IdentityModels.cs
+++ Rijschool/Rijschool/Models/IdentityModels.cs
@@ -22,7 +22,7 @@ namespace Rijschool.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("Rijschool", throwIfV1Schema: false)
         {
         }
 
